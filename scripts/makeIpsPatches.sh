6c3d4586a4505625726578502057cbb2fc67e3b0
Matt Greer <matt.e.greer@gmail.com>
2024-06-26 17:45:13 -0400

    ips tools for neorh

---
+++ scripts/makeIpsPatches.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+GAME='pbobblen'
+HACK='precise-controls'
+
+rm -rf ipsBinaries
+rm -rf ipsPatches
+
+mkdir ipsPatches
+mkdir ipsBinaries
+mkdir ipsBinaries/original
+mkdir ipsBinaries/hacked
+
+yarn restore;
+cp ${MAME_ROM_DIR}/${GAME}.zip ipsBinaries/original/
+
+yarn ts-node src/patchRom/main.ts src/patches/preciseControls.json
+cp ${MAME_ROM_DIR}/${GAME}.zip ipsBinaries/hacked/
+
+(cd ipsBinaries/original/ && unzip ${GAME}.zip)
+(cd ipsBinaries/hacked/ && unzip ${GAME}.zip)
+
+for f in `ls ipsBinaries/original/ -I ${GAME}.zip`; do
+    bf=`basename ${f}`
+
+    originalFile="ipsBinaries/original/${bf}"
+    hackedFile="ipsBinaries/hacked/${bf}"
+
+    originalSha=$(sha256sum "${originalFile}" | awk '{ print $1 }' )
+    hackedSha=$(sha256sum "${hackedFile}" | awk '{ print $1 }' )
+
+    echo "${bf} originalSha: ${originalSha}"
+    echo "${bf} hackedSha: ${hackedSha}"
+
+    if [ "${originalSha}" != "${hackedSha}" ]; then
+        echo "Creating ips for ${bf}"
+        yarn ts-node src/tools/makeIpsPatch.ts ipsBinaries/original/${bf} ipsBinaries/hacked/${bf} ipsPatches/${GAME}${HACK}.${bf}.ips
+    fi
+done
+
+(cd ipsPatches && zip ${GAME}${HACK}IpsPatches.zip *.ips)
